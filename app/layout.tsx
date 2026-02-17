c97764274cb6c127d09bd1e6f090c062e5ce6030
Etsero Raynaud <etseroraynaud@gmail.com>
2026-02-17 06:58:04 +0100

    Add real favicon.ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
+      { url: '/favicon.ico', type: 'image/x-icon' },
       { url: '/favicon-32.png', sizes: '32x32', type: 'image/png' },
       { url: '/favicon.png', sizes: '512x512', type: 'image/png' },
     ],
